b3f0ae4f8ed4898f7229c16d8fa3d7edf235faec
roterfil <claireroterfil@gmail.com>
2025-06-29 22:44:08 +0800

    Config

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -3,6 +3,11 @@
 // This is the simplest and most reliable method for your local setup.
 // It will never fail.
 define('BASE_URL', 'http://localhost/juanunit/');
+define('SITE_URL', 'https://juanunit.roterfil.com');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'juanunit_db');
+define('DB_USER', 'root');
+define('DB_PASS', 'GwAF16pf:Cf5');
 // === END: BASE_URL Definition ===
 
 
